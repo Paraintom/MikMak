de9f26c5c2f3c9a5de00238607e237b5ecea68e1
= <thomas.barles@gmail.com>
2012-12-01 11:59:27 +0000

    Give a pawn a player number (for chess there is two 'Q' and you should be able to distinct witch one belong to player one, and witch one belong to player two)

--- MikMakSolution/MikMakCommons/Commons/Pawn.cs
+++ MikMakSolution/MikMakCommons/Commons/Pawn.cs
@@ -14,6 +14,12 @@ namespace MikMak.Commons
         /// Could be 'Q' for Queen, as it could be 'B' for black...        
         /// </summary>
         public char Name { get; set; }
+
+        /// <summary>
+        /// There is two Queen, give the pawn's owner number
+        /// </summary>
+        public int PlayerOwner { get; set; }
+
         public Point Coord { get; set; }
 
         public Pawn(char name, int x, int y)
