3ab04bf90ee65a28e348fa90a706ec66f3a4fb05
OlivierM <martinet.olivier@gmail.com>
2012-12-17 22:54:11 +0100

    Mise en place du nouveau design

--- MikMakSolution/MikMak.WebFront/App_Start/BundleConfig.cs
+++ MikMakSolution/MikMak.WebFront/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace MikMak.WebFront
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery/jquery-ui-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                       "~/Scripts/bootstrap/bootstrap.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery/jquery.unobtrusive*",
                         "~/Scripts/jquery/jquery.validate*"));
@@ -21,26 +24,15 @@ namespace MikMak.WebFront
             bundles.Add(new ScriptBundle("~/bundles/mikmak").Include(
                         "~/Scripts/mikmak.gameSample.js"));
 
-            // Utilisez la version de développement de Modernizr pour développer et apprendre. Puis, lorsque vous êtes
-            // prêt pour la production, utilisez l’outil de génération sur http://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/other/modernizr-*"));
 
+            // Css
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap/css/bootstrap.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/bootstrapResponsive").Include("~/Content/bootstrap/css/bootstrap-responsive.min.css"));
+            
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
 
             
 
